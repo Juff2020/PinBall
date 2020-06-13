15848f220a56d9be73b6e2dd0afd32c5642cd3d7
Juff2020 <jeffreybtennant@gmail.com>
2020-06-13 10:33:12 +0800

    10:33am - 13.06.2020

--- PinBall-HomeOffice/Pinball-homeoffice/Assets/flipperScript.cs
+++ PinBall-HomeOffice/Pinball-homeoffice/Assets/flipperScript.cs
@@ -5,15 +5,35 @@ using UnityEngine;
 public class flipperScript : MonoBehaviour
 {
     public HingeJoint2D leftFlipperHingeJoint;
+    private JointMotor2D leftMotor2D;
+    public Rigidbody2D leftFlipperRigidBody2D;
+    public float leftFlipperMass;
+    public float leftFlipperMotorSpeed;
+
     public HingeJoint2D rightFlipperHingeJoint;
+    private JointMotor2D rightMotor2D;
+    public Rigidbody2D rightFlipperRigidBody2D;
+    public float rightFlipperMass;
+    public float rightFlipperMotorSpeed;
+
 
     public bool leftFlipperMotorOn;
     public bool rightFlipperMotorOn;
 
-        // Update is called once per frame
+
+    private void Start()
+    {
+        leftMotor2D = leftFlipperHingeJoint.motor;
+        leftMotor2D.motorSpeed = leftFlipperMotorSpeed;
+
+        rightMotor2D = rightFlipperHingeJoint.motor;
+        rightMotor2D.motorSpeed = rightFlipperMotorSpeed;
+    }
+
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("LeftFlipper"))
+        if (Input.GetButton("Left Flipper"))
         {
             leftFlipperHingeJoint.useMotor = true;
             leftFlipperMotorOn = true;
@@ -24,7 +44,7 @@ public class flipperScript : MonoBehaviour
             leftFlipperMotorOn = false;
         }
 
-        if (Input.GetButton("RightFlipper"))
+        if (Input.GetButton("Right Flipper"))
         {
             rightFlipperHingeJoint.useMotor = true;
             rightFlipperMotorOn = true;

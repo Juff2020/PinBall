3dac82b542658dc17aa0a5aa0e485909ee7f64b9
Juff2020 <jeffreybtennant@gmail.com>
2020-06-13 10:02:08 +0800

    13.06.2020

---
+++ PinBall-HomeOffice/Pinball-homeoffice/Assets/flipperScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flipperScript : MonoBehaviour
+{
+    public HingeJoint2D leftFlipperHingeJoint;
+    public HingeJoint2D rightFlipperHingeJoint;
+
+    public bool leftFlipperMotorOn;
+    public bool rightFlipperMotorOn;
+
+        // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("LeftFlipper"))
+        {
+            leftFlipperHingeJoint.useMotor = true;
+            leftFlipperMotorOn = true;
+        }
+        else
+        {
+            leftFlipperHingeJoint.useMotor = false;
+            leftFlipperMotorOn = false;
+        }
+
+        if (Input.GetButton("RightFlipper"))
+        {
+            rightFlipperHingeJoint.useMotor = true;
+            rightFlipperMotorOn = true;
+        }
+        else
+        {
+            rightFlipperHingeJoint.useMotor = false;
+            rightFlipperMotorOn = false;
+        }
+    }
+}
